c84e0e166e657c717de6530f09fe1ba41964bb39
nataliasviattseva <nataliasviattseva@gmail.com>
2021-10-15 21:04:33 +0200

    metod create renamed to create group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,8 +3,8 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.group.create(Group(name="newGroup", header="groupHeader", footer="groupFooter"))
+    app.group.create_group(Group(name="newGroup", header="groupHeader", footer="groupFooter"))
 
 
 def test_add_empty_group(app):
-    app.group.create(Group(name="", header="", footer=""))
+    app.group.create_group(Group(name="", header="", footer=""))
