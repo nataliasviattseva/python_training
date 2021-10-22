a1be46436c9623152f7efb9d203355a6b71b23be
nataliasviattseva <nataliasviattseva@gmail.com>
2021-10-22 17:06:37 +0200

    unused definitions were removed

--- fixture/session.py
+++ fixture/session.py
@@ -27,12 +27,10 @@ class SessionHelper:
         return wd.find_element_by_xpath("//div[@id='top']/form/b").text == "(" + username + ")"
 
     def ensure_logout(self):
-        wd = self.app.wd
         if self.is_logged_in():
             self.logout()
 
     def ensure_login(self, username, password):
-        wd = self.app.wd
         if self.is_logged_in():
             if self.is_logged_in_as(username):
                 return
