9d1ef828a68f1f3fd2c3193ce975a8ec06c37e76
nataliasviattseva <nataliasviattseva@gmail.com>
2021-11-18 12:49:57 +0100

    5. Strings > 16. implement command prompt options to specify browser type and opplication url

--- conftest.py
+++ conftest.py
@@ -8,7 +8,9 @@ fixture = None
 def app(request):
     global fixture
     if fixture is None:
-        fixture = Application()
+        browser = request.config.getoption("--browser")
+        base_url = request.config.getoption("--baseUrl")
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
             fixture = Application()
@@ -21,6 +23,10 @@ def stop(request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
-
     request.addfinalizer(fin)
     return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="https://localhost/addressbook/")
+
--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
@@ -7,12 +6,20 @@ from fixture.entry import EntryHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.entry = EntryHelper(self)
+        self.base_url=base_url
 
     def is_valid(self):
         try:
@@ -23,7 +30,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("https://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
