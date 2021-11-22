448b955abc7e4d23872e968049c066b6ad82d40c
nataliasviattseva <nataliasviattseva@gmail.com>
2021-11-22 14:49:24 +0100

    6. Files > 19: Implement test data load from file (json) some fixes

--- generator/entry.py
+++ generator/entry.py
@@ -1,4 +1,5 @@
 from model.entry import Entry
+from datetime import datetime
 import random
 import string
 import os.path
@@ -23,12 +24,16 @@ for o, a in opts:
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " " * 10 + string.digits
+    symbols = string.ascii_letters + string.digits + " " * 10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_number(maxlen):
+    symbols = string.digits
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
 def random_phone(maxlen):
-    symbols = string.digits + " " * 10 + "(" * 10 + ")" * 10 + "-" * 10
+    symbols = string.digits + " " + "(" + ")" + "-"
     return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
@@ -37,6 +42,12 @@ def random_email(maxlen1, maxlen2, maxlen3):
     return "".join([random.choice(symbols) for i in range(random.randrange(maxlen1))]) + \
            "@" + "".join([random.choice(symbols) for i in range(random.randrange(maxlen2))]) + \
            "." + "".join([random.choice(symbols) for i in range(random.randrange(maxlen3))])
+
+
+def random_month():
+    months_list = ["January", "February", "March", "April", "May", "June",
+                   "July", "August", "September", "October", "November", "December"]
+    return "".join([random.choice(months_list)])
 
 
 testdata = [Entry(first_name="",
@@ -57,12 +68,12 @@ testdata = [Entry(first_name="",
                   address2="",
                   phone2_home="",
                   notes="",
-                  bday="-",
-                  bmonth="-",
-                  byear="-",
-                  aday="-",
-                  amonth="-",
-                  ayear="-")
+                  bday=None,
+                  bmonth=None,
+                  byear="",
+                  aday=None,
+                  amonth=None,
+                  ayear="")
             ] + [Entry(first_name=random_string("FirstName", 10),
                        middle_name=random_string("MiddleName", 10),
                        last_name=random_string("LastName", 10),
@@ -77,16 +88,16 @@ testdata = [Entry(first_name="",
                        email1=random_email(10, 10, 3),
                        email2=random_email(10, 10, 3),
                        email3=random_email(10, 10, 3),
-                       homepage="www.homepage.com",
+                       homepage=random_string("www", 200),
                        address2=random_string("456 address", 50),
                        phone2_home=random_phone(10),
                        notes=random_string("notes", 50),
-                       bday="1",
-                       bmonth="January",
-                       byear="2000",
-                       aday="5",
-                       amonth="February",
-                       ayear="2001")
+                       bday=str(random.choice(range(0, 32))),
+                       bmonth=random_month,
+                       byear=random.choice(range(1900, datetime.now().year)),
+                       aday=str(random.choice(range(0, 32))),
+                       amonth=random_month,
+                       ayear=random.choice(range(1900, datetime.now().year)))
                  for i in range(n)
                  ]
 
