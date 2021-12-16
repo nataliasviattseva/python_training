ddcac936d94a24594d28437975eb30c66f7aa414
nataliasviattseva <nataliasviattseva@gmail.com>
2021-12-16 15:51:33 +0100

    7. DB > 21: Changed tests for check information from home page, added assertion with information from db

--- test/test_check_contact.py
+++ test/test_check_contact.py
@@ -1,6 +1,7 @@
 import re
 
 
+# check one entry from home page and edit page
 # def test_contact_on_home_page(app):
 #     contact_from_home_page = app.entry.get_entries_list()[0]
 #     contact_from_edit_page = app.entry.get_entry_info_from_edit_page(0)
@@ -21,7 +22,7 @@ def test_check_contact_on_home_page_with_contacts_from_db(app, db):
         contact_from_home_page = list[entry.id]
         assert contact_from_home_page.last_name == entry.last_name.rstrip()
         assert contact_from_home_page.first_name == entry.first_name
-        assert contact_from_home_page.address == entry.address
+        assert clear(contact_from_home_page.address) == clear(entry.address)
         assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(entry)
         assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(entry)
 
@@ -41,4 +42,3 @@ def merge_emails_like_on_home_page(entry):
     return "\n".join(filter(lambda x: x != "",
                             filter(lambda x: x is not None,
                                    [entry.email1, entry.email2, entry.email3])))
-
