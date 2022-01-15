cedfc07b8b3d73a3aa875c4d3cf6e6b6a30ff0e7
nataliasviattseva <nataliasviattseva@gmail.com>
2022-01-15 02:24:55 +0100

    revert changes

--- test/test_entry_in_group.py
+++ test/test_entry_in_group.py
@@ -3,17 +3,17 @@ from model.group import Group
 import random
 
 
-def test_add_entry_in_group(app, orm):
-    if len(orm.get_groups_without_entries()) == 0:
+def test_add_entry_in_group(app, orm, db):
+    if len(db.get_groups_without_entries()) == 0:
         app.group.create_group(Group(name="test_group"))
 
-    if len(orm.get_entries_not_in_any_group()) == 0:
+    if len(db.get_entries_not_in_any_group()) == 0:
         app.entry.create_entry(Entry(first_name="test_entry"))
 
-    groups = orm.get_groups_without_entries()
+    groups = db.get_groups_without_entries()
     selected_group = random.choice(groups)
 
-    entries = orm.get_entries_not_in_any_group()
+    entries = db.get_entries_not_in_any_group()
     selected_entry = random.choice(entries)
 
     app.entry.add_entry_in_group(selected_entry.id, selected_group.id)
@@ -23,7 +23,7 @@ def test_add_entry_in_group(app, orm):
     assert selected_entry in entries_in_group
 
 
-def test_delete_entry_from_group(app, orm):
+def test_delete_entry_from_group(app, orm, db, check_ui):
     if len(orm.get_groups_list()) == 0:
         initial_group = Group(name="test_group")
         app.group.create_group(initial_group)
